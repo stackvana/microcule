a9560aece95d52e382ec2dfa98ce4bb5c4b5c469
Marak <marak.squires@gmail.com>
2016-08-26 05:48:30 -0400

    [api] Added ability to chain multiple services   * Multiple services can now be run in series   * Service output is concated to response

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var stack = {};
 stack.spawn = require('./lib/spawn');
+stack.chainServices = require('./lib/chainServices');
 stack.viewPresenter = require('./lib/viewPresenter');
 stack.validateRequestParams = require('./lib/validateRequestParams');
 module.exports = stack;
