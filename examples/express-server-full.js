c2be7997ddaf1eb2a198a81ff68a1cf24dda03d7
Marak <marak.squires@gmail.com>
2016-08-26 04:50:04 -0400

    [api] Added optional mschema for request params

--- examples/express-server-full.js
+++ examples/express-server-full.js
@@ -2,13 +2,23 @@ var express = require('express');
 var app = express();
 var stack = require('../');
 
+var mp = require('merge-params');
+
 // could also be node service / php / python / etc
 var bashService = 'echo "hello bash"';
+
+app.use(mp);
 
 app.use(
   stack.spawn({
   code: bashService, 
   language: "bash",
+  schema: {
+    "foo": {
+      "type": "string",
+      "required": true
+    }
+  },
   view: "this is a custom view {{hook.output}}",
   presenter: function viewPresenter (opts, cb) {
     var $ = this.$;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var stack = {};
 stack.spawn = require('./lib/spawn');
 stack.viewPresenter = require('./lib/viewPresenter');
+stack.validateRequestParams = require('./lib/validateRequestParams');
 module.exports = stack;
