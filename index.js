a9a3d374e0649113be8b32459195b9ceea3cb045
Marak <marak.squires@gmail.com>
2016-09-08 14:40:46 -0400

    [refactor] Switch to requireServiceSync   * No longer using async requires     * Easier API     * sync require isn't being used after start   * Fixes issues with View-Presenter     * Now allows presenter as string   * Fixes issue with legacy API properties   * Adds additional error handler in process spawn     * Ensure next() callback fires for all errors     * Ensure next() is called on module missing

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ stack.spawn = require('./lib/spawn');
 stack.chainServices = require('./lib/chainServices');
 stack.viewPresenter = require('./lib/viewPresenter');
 stack.validateRequestParams = require('./lib/validateRequestParams');
+stack.requireService = require('./lib/requireService');
+stack.requireServiceSync = require('./lib/requireServiceSync');
 module.exports = stack;
