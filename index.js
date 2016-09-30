c883e3ee94d0390d2e4fc26dcf58d51f4ccee903
Marak <marak.squires@gmail.com>
2016-09-30 03:11:32 -0400

    [fix] [api] Add plugins to `stack` scope   * Use `bodyParser` in request params test

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ stack.viewPresenter = require('./lib/viewPresenter');
 stack.validateRequestParams = require('./lib/validateRequestParams');
 stack.requireService = require('./lib/requireService');
 stack.requireServiceSync = require('./lib/requireServiceSync');
+
+stack.plugins = {
+  "bodyParser": require('./lib/plugins/bodyParser'),
+  "logger": require('./lib/plugins/logger'),
+  "mschema": require('./lib/plugins/mschema')
+};
+
 module.exports = stack;
