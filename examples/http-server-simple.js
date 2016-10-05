2e5227a1eea2404fd7098c4873d2d364b57c2fa3
Marak <marak.squires@gmail.com>
2016-10-04 22:38:55 -0400

    [dist] Update express and http server examples   * Now using stack v3.0.0 APIs   * Removed viewPresenter middlewares

--- examples/http-server-simple.js
+++ examples/http-server-simple.js
@@ -1,20 +1,24 @@
 var stack = require('../');
 var http = require('http');
 
-var handler = function testService (req, res) {
-  res.write('hello node!');
-  res.end();
+var service = function testService (opts) {
+  opts.res.write('hello node!');ddd
+  opts.res.end();
 };
 
-/* could also be bash or any supported language 
-  var handler = stack.spawn({
-    code: 'echo "hello bash";', 
-    language: "bash"
-  });
-*/
+/* could also be bash or any supported language */
+var handler = stack.spawn({
+  code: service,
+  language: "javascript"
+});
 
 var server = http.createServer(function(req, res){
-  handler(req, res);
+  handler(req, res, function (err) {
+    if (err) {
+      return res.end(err.message);
+    }
+    console.log('request completed', req.url);
+  });
 });
 
 server.listen(3000, function () {
