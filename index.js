ff0b2f7ec5b409120dc1b4bddcb07f8de8e4ba28
Marak <marak.squires@gmail.com>
2016-10-04 21:58:31 -0400

    [refactor] Remove chainServices code   * Should be plugin or separate project

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var stack = {};
 stack.spawn = require('./lib/spawn');
-stack.chainServices = require('./lib/chainServices');
 stack.viewPresenter = require('./lib/viewPresenter');
 stack.validateRequestParams = require('./lib/validateRequestParams');
 stack.requireService = require('./lib/requireService');
