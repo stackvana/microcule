c16c387e5e0f99ce8a4b58506687a68c74187a9e
Marak <marak.squires@gmail.com>
2016-12-23 18:44:21 -0500

    [examples] Add image resize example

---
+++ examples/services/image/index.js
@@ -0,0 +1,15 @@
+// GraphicsMagick fully supported
+var gm = require('gm');
+var request = require('request');
+
+module['exports'] = function imageResize (hook) {
+  // grab an image stream using request
+  var stream = request('https://hook.io/img/robotcat.png');
+  hook.res.writeHead(200, { 'Content-Type': 'image/png' });
+  // create a gm resize stream and pipe to response
+  gm(stream)
+    .options({imageMagick: true })
+    .resize(150, 150)
+    .stream()
+    .pipe(hook.res);
+};
