2b84a57bed3f467e9766ffdb49f8bead6e8b7da9
Marak <marak.squires@gmail.com>
2017-01-09 22:55:11 -0500

    [api] Added logging and object parameters to lua   * Adds simple `log` method for lua services   * Adds `Hook` object for lua services

--- examples/services/echo/echo.lua
+++ examples/services/echo/echo.lua
@@ -1,3 +1,9 @@
+local json = require('json')
 print ("Hello, this is a Lua script.")
-print ("Hook_params_* is populated with request parameters")
-print ("Hook_params_foo: " .. Hook_params_foo)
+print ("Hook['params'] is populated with request parameters")
+print ("Hook['env'] is populated with env variables")
+print(json.stringify(Hook['params']))
+print(json.stringify(Hook['env']))
+
+-- gets sent to logging handler
+log('Hook params', Hook['params'])
