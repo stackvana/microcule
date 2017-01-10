1c6398d14524136b5725468cdc84658d22c56757
Marak <marak.squires@gmail.com>
2017-01-10 00:28:57 -0500

    [api] Added logging method to python   * Adds simple log method for all python services   * More advanced logging for python is planned

--- examples/services/echo/echo.py
+++ examples/services/echo/echo.py
@@ -1,4 +1,8 @@
-import pprint
+import pprint, sys, json
 print "Hello, this is a Python script."
 print "Hook['params'] is populated with request parameters"
 pprint.pprint(Hook['params'])
+print "Hook['env'] is populated with env variables"
+pprint.pprint(Hook['env'])
+log('hello logs')
+log(Hook['params'])
