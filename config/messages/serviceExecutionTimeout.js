8f4c810d5786e268b08614667239a8f9e85a84d9
Marak <marak.squires@gmail.com>
2017-02-16 16:23:50 -0500

    [api] [minor] [fix] Typo in timeout error

--- config/messages/serviceExecutionTimeout.js
+++ config/messages/serviceExecutionTimeout.js
@@ -5,7 +5,7 @@ module['exports'] = function serviceExecutionTimeoutMessage (seconds) {
   str += seconds;
   str += ' complete.\n\n';
   str += 'A delay of this long may indicate there is an error in the source code for the Service. \n\n';
-  str += 'If there are no errors and the Servuce requires more than ';
+  str += 'If there are no errors and the Service requires more than ';
   str += seconds;
   str += ' seconds to execute, you can increase SERVICE_MAX_TIMEOUT variable.';
   return str;  
