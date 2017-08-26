f599105dc36df6a869262e9046d7438aba18b20c
Janaka A <janaka@hudl.com>
2017-08-26 10:37:26 +0100

    Add all primary logic for .NET Core support

---
+++ examples/services/hello-world/dotnet-hello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from .NET Core!");
+        }
+    }
+}
