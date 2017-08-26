88d0db1b06b2512f643ce41881b456bed7beccc5
Marak <marak.squires@gmail.com>
2017-08-25 21:22:31 -0400

    [config] [fix] Don’t use SSL/HTTPS by default

--- config/index.js
+++ config/index.js
@@ -4,11 +4,11 @@ module.exports = {
   http: {
     port: 3000,
     host: "0.0.0.0",
-    https: true, // set to `true` to enable SSL server. cert, key, and ca will be required.
+    https: false, // set to `true` to enable SSL server. cert, key, and ca will be required.
     key: fs.readFileSync(__dirname + "/ssl/server-key.pem").toString(),
     cert: fs.readFileSync(__dirname + "/ssl/server-crt.pem").toString(),
     ca: [fs.readFileSync(__dirname + '/ssl/ca-crt.pem').toString()],
-    sslRequired: true, // redirects all http traffic to https, optional
+    sslRequired: false, // redirects all http traffic to https, optional
     onlySSL: false // will only start https server with no unprotected http interface, optional
   },
   SERVICE_MAX_TIMEOUT: 10000,
