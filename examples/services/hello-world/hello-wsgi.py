eee41eac6e20c64377e99d1f968108b6dbce21d8
Marak <marak.squires@gmail.com>
2017-08-31 15:51:44 -0400

    [examples] Update python wsgi API

--- examples/services/hello-world/hello-wsgi.py
+++ examples/services/hello-world/hello-wsgi.py
@@ -1,6 +1,5 @@
 import pprint
 import logging
-import microcule
 
 log = logging.getLogger('echo-py')
 
@@ -8,6 +7,3 @@ def app(environ, start_response):
     start_response('200 OK', [('content-type', 'text/plain')])
     res = ["hello world"]
     return '\n'.join(res)
-
-if __name__ == '__main__':
-    microcule.wsgi(Hook).run(app)
