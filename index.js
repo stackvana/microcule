3252e33ed1d8133cabd63be25a6b8558b3ae9263
Marak <marak.squires@gmail.com>
2019-04-12 20:20:23 -0400

    [api] [refactor] Remove cron plugin from project   - Cron is not a direct concern of streaming functions   - Plugins should be directly related to streams   - Cron implementation has moved back into hook.io

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ microcule.requireServiceSync = require('./lib/requireServiceSync');
 microcule.plugins = {
   "bodyParser": require('./lib/plugins/bodyParser'),
   "compile": require('./lib/plugins/compile'),
-  "cronScheduler": require('./lib/plugins/cronScheduler'),
   "logger": require('./lib/plugins/logger'),
   "mschema": require('./lib/plugins/mschema'),
   "rateLimiter": require('./lib/plugins/rateLimiter'),
